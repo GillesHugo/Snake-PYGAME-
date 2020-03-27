1a47fd05cfb9883dcefc018b478a0969e1c93a62
LaymajouxClement <laymajouxclement@gmail.com>
2020-03-27 18:12:02 +0100

    Basic window

---
+++ main.py
@@ -0,0 +1,31 @@
+import pygame,sys       #import modules
+
+screen_w = 500
+screen_h = 600
+
+colors = {              #definition of basics colors
+    "black":(0,0,0),
+    "white":(255,255,255),
+    "red":(255,0,0),
+    "blue":(0,255,0),
+    "green":(0,0,255),
+    }
+
+pygame.init()
+window = pygame.display.set_mode((screen_h,screen_w)) #window creation
+game = True
+
+try:
+    while game:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT or pygame.key.get_pressed()[pygame.K_ESCAPE]:  #Escape or exit  is pressed
+                game = False #end of the game
+
+        window.fill(colors["black"])
+        pygame.display.flip()
+
+except:
+    print("Exit error")
+
+    pygame.quit()
+    sys.exit()
