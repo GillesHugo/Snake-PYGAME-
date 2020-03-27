326679864a01cf3bae98a92a1dbce5d48afdf623
GillesHugo <55876653+GillesHugo@users.noreply.github.com>
2020-03-27 20:28:10 +0100

    Snake_again
    g creer le carrée ki bougent

--- main.py
+++ main.py
@@ -18,10 +18,27 @@ game = True
 
 grass = pygame.image.load("grass.png")
 
+snake = pygame.image.load("snake.png")
 
+
+position_snake = snake.get_rect()
+window.blit(snake, (100, 100))
 
 try:
     while game:
+
+        if pygame.key.get_pressed()[pygame.K_UP]:
+            position_snake = position_snake.move(0,-1)
+        if pygame.key.get_pressed()[pygame.K_DOWN]:
+            position_snake = position_snake.move(0,1)
+        if pygame.key.get_pressed()[pygame.K_LEFT]:
+            position_snake = position_snake.move(-1,0)
+        if pygame.key.get_pressed()[pygame.K_RIGHT]:
+            position_snake = position_snake.move(1,0)
+
+
+
+        
         for event in pygame.event.get():
             if event.type == pygame.QUIT or pygame.key.get_pressed()[pygame.K_ESCAPE]:  #Escape or exit  is pressed
                 game = False #end of the game
@@ -30,7 +47,8 @@ try:
         for i in range (0,50):
             for j in range (0, 32):
                 window.blit(grass, (i*16,j*16))
-                
+
+        window.blit(snake, position_snake)
         pygame.display.flip()
 
 except:
